662544d030764610e89facd177b69d942d8fee2f
yooonwodyd <103253736+yooonwodyd@users.noreply.github.com>
2023-03-21 13:33:13 +0900

    [Gold II] Title: 괄호의 값 비교, Time: 116 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Gold/22343. 괄호의 값 비교/괄호의 값 비교.py
+++ 백준/Gold/22343. 괄호의 값 비교/괄호의 값 비교.py
@@ -1,42 +1,35 @@
+import sys
 
-def calcuFx(a):
+t=int(sys.stdin.readline())
 
-    stack = []
-    count = 0
-    while count != len(a):
-        if a[count] == '(':
-            stack.append(a[count])
-        elif stack and a[count] == ')' and stack[-1] == '(':
-            stack[-1] = 1
-
-        elif len(stack) > 1 and a[count] == ')' and stack[-1] != '(' and stack[-2] == '(':
-            ## 숫자 뒤에 괄호가 나왔다면 숫자 * 2
-            stack[-2] = stack[-1] * 2
-            del stack[-1]
-        while True:
-            if len(stack) > 1 and stack[-2] != '(' and stack[-1] != '(':
-                ## 길이가 2보다 큰데 스택의 맨 위에가 둘다 숫자라면
-                stack[-2] += stack[-1]
-                del stack[-1]
-            else:
-                break
-        count += 1
-
-    return stack[0]
-
-
-
-
-
-
-T = int(input())
-
-for i in range(T):
-    A = calcuFx(input())
-    B = calcuFx(input())
-    if A == B:
-        print('=')
-    elif A < B:
-        print("<")
-    elif A > B:
+for i in range(t):
+    a=sys.stdin.readline().rstrip()
+    l=len(a)
+    v=1
+    ans=0
+    ans2=0
+    for j in range(l):
+        if a[j]=='(':
+            v*=2
+        elif a[j-1]=='(' and a[j]==')':
+            ans+=v//2
+            v//=2
+        else:
+            v//=2
+    a=sys.stdin.readline().rstrip()
+    l=len(a)
+    v=1
+    for j in range(l):
+        if a[j]=='(':
+            v*=2
+        elif a[j-1]=='(' and a[j]==')':
+            ans2+=v//2
+            v//=2
+        else:
+            v//=2
+    if ans>ans2:
         print('>')
+    elif ans<ans2:
+        print('<')
+    else:
+        print('=')
