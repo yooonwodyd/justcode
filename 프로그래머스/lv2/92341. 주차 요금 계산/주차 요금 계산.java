06bd45d4ef9f8e9ca50f78e0eec16f433e67ad80
yooonwodyd <103253736+yooonwodyd@users.noreply.github.com>
2023-06-10 13:30:58 +0900

    [level 2] Title: 주차 요금 계산, Time: 1.73 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/lv2/92341. 주차 요금 계산/주차 요금 계산.java
+++ 프로그래머스/lv2/92341. 주차 요금 계산/주차 요금 계산.java
@@ -1,59 +1,57 @@
 import java.util.*;
-
 class Solution {
     public int[] solution(int[] fees, String[] records) {
-        int[] answer = {};
+        int[][] cars = new int[10000][2];
+        HashMap<Integer,Boolean> map = new HashMap<>();
         
-        Map<String, String> map = new HashMap<>();
-        Map<String, Integer> feeMap = new HashMap<>();
-        
-        for(int i = 0; i < records.length; i++){
-            feeMap.put(records[i].split(" ")[1], 0);
-        }
-        
-        for(int i = 0; i < records.length; i++){
-            String[] infos = records[i].split(" ");
-            
-            if(map.containsKey(infos[1])){
-                String[] inTime = map.remove(infos[1]).split(":");
-                String[] outTime = infos[0].split(":");
-                
-                int hour = Integer.parseInt(outTime[0]) - Integer.parseInt(inTime[0]);
-                int minute = Integer.parseInt(outTime[1]) - Integer.parseInt(inTime[1]);
-                
-                feeMap.replace(infos[1], feeMap.get(infos[1]) + 60 * hour + minute);
-                
-            }else{
-                map.put(infos[1], infos[0]); // 차 번호, 시간
+        for(String record: records){
+            String[] arr = record.split(" ");
+            int time = cal_time(arr[0]);
+            int num = Integer.parseInt(arr[1]);
+            map.put(num,true);
+            if(cars[num][1] == 0){
+                cars[num][0] -= time;
+                cars[num][1] = 1;
+            }
+            else{
+                cars[num][0] += time;
+                cars[num][1] = 0;
             }
         }
+        List<Integer> l = new ArrayList<Integer>();
         
-        for(String key : map.keySet()){
-            String[] inTime = map.get(key).split(":");
-            
-            int hour = 23 - Integer.parseInt(inTime[0]);
-            int minute = 59 -Integer.parseInt(inTime[1]);
-            
-            feeMap.replace(key, feeMap.get(key) + 60 * hour + minute);
-        }
-        
-        List<Map.Entry<String, Integer>> list = new ArrayList(feeMap.entrySet());
-        Collections.sort(list, (o1, o2) -> {
-            return Integer.parseInt(o1.getKey()) > Integer.parseInt(o2.getKey())?1 : 
-            Integer.parseInt(o1.getKey()) < Integer.parseInt(o2.getKey())?-1 : 0;
-        });
-        
-
-        answer = new int[list.size()];
-        
-        for(int i = 0; i < answer.length; i++){
-            if(list.get(i).getValue() > fees[0]){
-                answer[i] = fees[1] + (int) Math.ceil((list.get(i).getValue() - fees[0]) / (double)fees[2]) * fees[3];
-            }else{
-                answer[i] = fees[1];
+        for(int i = 0; i < 10000; i++){
+            if(cars[i][0] == 0 && map.containsKey(i) == false){
+                continue;
+            }
+            if(cars[i][1] == 1){
+                cars[i][0] += cal_time("23:59");
+            }
+            if(cars[i][0] <= fees[0]){
+                l.add(fees[1]);
+            }
+            else{
+                if((cars[i][0] - fees[0]) % fees[2] != 0){
+                    
+                    l.add(fees[1] + ((cars[i][0] - fees[0])/fees[2]+1) * fees[3]);
+                }
+                else{
+                    l.add(fees[1] + (cars[i][0] - fees[0])/fees[2] * fees[3]);
+                }  
             }
         }
-        
+        int[] answer = new int[l.size()];
+       
+        for(int i = 0; i < l.size(); i++){
+            answer[i] = l.get(i);
+        }
         return answer;
     }
+    
+    
+    public int cal_time(String record){
+        String[] str = record.split(":");
+        return Integer.parseInt(str[0]) * 60 + Integer.parseInt(str[1]);
+    }
+    
 }
